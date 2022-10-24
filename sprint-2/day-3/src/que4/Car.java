f31599cc4aa4feaa8fa8e8dbcef5f73eac9a77de
Kundan-10 <kundanwakudkar10@gmail.com>
2022-10-24 18:46:04 +0530

    s2day-3

---
+++ sprint-2/day-3/src/que4/Car.java
@@ -0,0 +1,16 @@
+package que4;
+
+public class Car {
+	String model;
+	String companyName;
+	String color;
+	Engine engine;
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
